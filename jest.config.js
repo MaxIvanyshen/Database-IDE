3d3bea932af5407d0ea7a3016c6755786e9809b0
MaxIvanyshen <maxivanyshen@gmail.com>
2023-04-07 11:11:14 +0300

    add function findMany() to mongoDAO

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    bail: 3,
-    transform: {'^.+\\.ts?$': 'ts-jest'},
-    testEnvironment: 'node',
-    testRegex: '/test/.*\\.(test|spec)?\\.(ts|tsx)$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
-};
